29ee17481993f3bff98c45ea02c7cd6241c3f61a
Oswaldo Santiago Ortiz Cardenas <143229490+Oswaldo-Santiago-Ortiz-Cardenas@users.noreply.github.com>
2023-09-22 00:31:48 -0500

    Add files via upload

---
+++ Ejercicio 04/Ejercicio 04/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Ingrese el número límite: ");
+int numl = int.Parse(Console.ReadLine());
+Console.WriteLine("==========================");
+Console.WriteLine("Escoja una opción:");
+Console.WriteLine("1: Números pares ");
+Console.WriteLine("2: Números impares");
+Console.WriteLine("3: Factorial");
+Console.Write("->");
+int opc = int.Parse(Console.ReadLine());
+switch (opc)
+{
+    case 1:
+        for (int i = 2 ; i <= numl ; i +=2 )
+        {
+            Console.Write(i + ", ");
+        }
+        break;
+    case 2:
+        for (int i = 1 ; i <= numl ; i += 2 )
+        {
+            Console.Write(i + ", ");
+        }
+        break;
+    case 3:
+        long fac = 1;
+        for (int i = 1 ; i <= numl ; i++)
+        {
+            fac = fac * i;
+        }
+        Console.WriteLine(fac);
+        break;
+}
+Console.ReadKey();
